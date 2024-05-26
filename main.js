e5a72edc96a0e871f7aaeaa23dd302522d5251b3
Kelby Rogers <kelby_rogers@trimble.com>
2024-05-27 10:12:17 +1200

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// Three.js - Fundamentals
+// from https://threejs.org/manual/examples/fundamentals.html
+
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+function main() {
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  const scene = new THREE.Scene();
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 }); // greenish blue
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  dirLight.position.set(-3, 3, 10);
+  dirLight.layers.enableAll();
+  scene.add(dirLight);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
+
+  const axesHelper = new THREE.AxesHelper(1.3, 1.3, 1.3);
+  scene.add(axesHelper);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minDistance = 1;
+  controls.maxDistance = 20;
+
+  window.addEventListener("resize", onWindowResize);
+
+  function onWindowResize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+  }
+
+  draw();
+
+  function draw() {
+    requestAnimationFrame(draw);
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    renderer.setSize(width, height);
+    renderer.render(scene, camera);
+  }
+}
+
+main();
