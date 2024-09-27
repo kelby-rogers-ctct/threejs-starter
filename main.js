573e09b377353e4e2b236a81821283df1435d7cc
Kelby Rogers <kelby_rogers@trimble.com>
2024-09-27 14:53:42 +1200

    place hex on test strip

--- main.js
+++ main.js
@@ -25,9 +25,28 @@ async function main() {
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambientLight);
 
-  const gltf = await loadGLTF();
-  gltf.scene.position.set(0, 0, 0);
-  scene.add(gltf.scene);
+  const kettlewell = await loadGLTF("./kettlewell.glb");
+  kettlewell.scene.position.set(0, 0, 0);
+  scene.add(kettlewell.scene);
+
+  const hex = await loadGLTF("./hex.gltf");
+  hex.scene.position.set(90, 30, -100);
+  const scaleFactor = 6;
+  hex.scene.scale.set(scaleFactor, scaleFactor, scaleFactor);
+  hex.scene.traverse((node) => {
+    if (node.isMesh && node.material) {
+      // Check for both normal material and array of materials
+      if (Array.isArray(node.material)) {
+        node.material.forEach((material) => {
+          material.color.set(0xffff00);
+        });
+      } else {
+        node.material.color.set(0xffff00);
+      }
+    }
+  });
+  hex.scene.rotateY(Math.PI / 2.7);
+  scene.add(hex.scene);
 
   const axesHelper = new THREE.AxesHelper(1.3, 1.3, 1.3);
   scene.add(axesHelper);
@@ -38,11 +57,14 @@ async function main() {
 
   window.addEventListener("resize", onWindowResize);
 
-  async function loadGLTF() {
+  async function loadGLTF(path) {
     const loader = new GLTFLoader();
-    const gltf = await loader.loadAsync("./kettlewell.glb");
+    const lights = [];
+    const gltf = await loader.loadAsync(path);
     gltf.scene.traverse((node) => {
-      if (node.isMesh && node.material) {
+      if (node.isLight) {
+        lights.push(node);
+      } else if (node.isMesh && node.material) {
         // Check for both normal material and array of materials
         if (Array.isArray(node.material)) {
           node.material.forEach((material) => {
@@ -51,6 +73,12 @@ async function main() {
         } else {
           node.material.side = THREE.DoubleSide;
         }
+      }
+    });
+    // Remove lights from their parents after traversal
+    lights.forEach((light) => {
+      if (light.parent) {
+        // light.parent.remove(light);
       }
     });
     return gltf;
@@ -70,9 +98,19 @@ async function main() {
     requestAnimationFrame(draw);
     const width = window.innerWidth;
     const height = window.innerHeight;
-
+    // hex.scene.position.x -= 0.1;
+    moveForward(hex.scene, -0.1);
     renderer.setSize(width, height);
     renderer.render(scene, camera);
+  }
+
+  function moveForward(object, distance) {
+    // Get the current direction the object is facing
+    const direction = new THREE.Vector3();
+    object.getWorldDirection(direction);
+
+    // Move the object along its forward direction
+    object.position.add(direction.multiplyScalar(distance));
   }
 }
 
